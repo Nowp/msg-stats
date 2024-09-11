b1b04f292c0fdd0b6df38bad9f6e436053df56b6
Noé <noepec1@gmail.com>
2024-09-11 22:00:01 +0200

    feat(cli): Commande line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use crate::messenger::MessengerConversation;
 
 use crate::base::adapter::{ConversationConverter, ConversationLoader, MergeImportFiles};
-use crate::base::unnest::Unnest;
-use itertools::Itertools;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::Error;
 use std::fs;
+use clap::{arg, Parser};
 
 mod messenger;
 mod base;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// URL of the Postgres database holding the schema</br>postgres://user:password@address:\[port]/database
+    #[arg(short, long)]
+    destination: String,
+
+    /// Max connections of the connection pool
+    #[arg(short, long, default_value_t=1)]
+    max_connections: u32,
+
+    /// Folder containing files to import
+    #[arg(short, long)]
+    input: String,
+}
 
 fn parse_file(path: String) -> serde_json::Result<MessengerConversation> {
     let content: String = fs::read_to_string(path).expect("Cannot read file");
@@ -18,39 +33,29 @@ fn parse_file(path: String) -> serde_json::Result<MessengerConversation> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tokio::spawn(async {
-        // Create a connection pool
-        let pool = PgPoolOptions::new()
-            .max_connections(10)
-            .connect("postgres://postgres:messengerinput@localhost/ouaislesboys").await?;
+    let args = Args::parse();
 
-        let paths: Vec<String> = fs::read_dir("./messages/fixed")?
-            .into_iter()
-            .filter_map(|x|
-                match x {
-                    Ok(dir) => {
-                        if dir.metadata().unwrap().is_file() && dir.file_name().to_str().unwrap().contains(".json") { Some(dir.path()) } else { None }
-                    }
-                    Err(_) => { panic!("Panic") }
-                }
-            )
-            .map(|path| path.to_str().unwrap().to_string())
-            .collect();
+    // Create a connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(args.max_connections)
+        .connect(&*args.destination).await?;
 
-        let mut conversation = paths.into_iter()
-            .map(|path| parse_file(path).unwrap())
-            .collect::<Vec<MessengerConversation>>()
-            .into_iter()
-            .merge_import_files()
-            .map(MessengerConversation::convert)
-            .unwrap();
+    let paths: Vec<String> = fs::read_dir(args.input)?
+        .into_iter()
+        .map(|path| path.unwrap().path().to_str().unwrap().to_string())
+        .collect();
 
+    let conversation = paths.into_iter()
+        .map(|path| parse_file(path).unwrap())
+        .collect::<Vec<MessengerConversation>>()
+        .into_iter()
+        .merge_import_files()
+        .map(MessengerConversation::convert)
+        .unwrap();
 
-        conversation.load_participants(&pool).await?;
-        conversation.load_messages(&pool).await?;
-        conversation.load_reactions(&pool).await?;
+    conversation.load_participants(&pool).await?;
+    conversation.load_messages(&pool).await?;
+    conversation.load_reactions(&pool).await?;
 
-        Ok::<(), Error>(())
-    }).await.expect("Et bah non").expect("SQL error");
     Ok(())
 }
